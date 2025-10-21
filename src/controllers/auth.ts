42ca6e60ee62bdb6fd8259f5fd89c5f26e40a6de
leesinma1214 <revolver1214@gmail.com>
2025-10-21 04:12:03 +0000

    Generate random token for user verification

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -5,6 +5,15 @@ export const generateAuthLink: RequestHandler = (req, res) => {
   // Generate authentication link
   // And send that link to the users email address
 
+  /* 1. Generate Unique token for every users.
+    2. Store that token securely inside the database
+    so that we can validate it in the future.
+    3. Create a link which include that secure token and user information.
+    4. Send that link to users email address.
+    5. Notify user to look inside the email to get login link.*/
+
+  const randomToken = crypto.randomBytes(36).toString("hex");
+
   console.log(req.body);
 
   res.json({ ok: true });
